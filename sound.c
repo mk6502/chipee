00dc33bda4b58944bb72dad842063ffaa7915f81
Mike Staszel <mike@mikestaszel.com>
2019-12-30 21:19:56 -0600

    sound support

--- sound.c
+++ sound.c
@@ -6,7 +6,7 @@
 
 void audio_callback(void *user_data, unsigned char *raw_buffer, int bytes) {
     double sample_rate = 44100.0;
-    int amplitude = 10000;
+    int amplitude = 28000;
 
     Sint16 *buffer = (Sint16 *) raw_buffer;
     int length = bytes / 2; // 2 bytes per sample for AUDIO_S16SYS
@@ -22,10 +22,12 @@ void beep_sound() {
     int sample_nr = 0;
 
     SDL_AudioSpec want;
+    SDL_zero(want);
+
     want.freq = 44100; // number of samples per second
-    want.format = AUDIO_S16SYS; // sample type (here: signed short i.e. 16 bit)
-    want.channels = 1; // only one channel
-    want.samples = 1024; // buffer-size
+    want.format = AUDIO_F32;
+    want.channels = 2;
+    want.samples = 2048; // buffer-size
     want.callback = audio_callback; // function SDL calls periodically to refill the buffer
     want.userdata = &sample_nr; // counter, keeping track of current sample number
 
@@ -35,7 +37,7 @@ void beep_sound() {
     if (want.format != have.format) SDL_LogError(SDL_LOG_CATEGORY_AUDIO, "Failed to get the desired AudioSpec");
 
     SDL_PauseAudio(0); // start playing sound
-    SDL_Delay(2); // wait while sound is playing
+    SDL_Delay(25); // wait while sound is playing
     SDL_PauseAudio(1); // stop playing sound
 }
 
